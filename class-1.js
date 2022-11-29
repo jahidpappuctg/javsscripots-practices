26e6212993c464355fd8f1c24aebdca4e79360dd
jahidpappuctg <jahidpappublog@gmail.com>
2022-11-29 10:23:31 +0600

    first commit

---
+++ class-1.js
@@ -0,0 +1,133 @@
+// // var  firstName = "Jahid";
+
+
+
+// // console.log("name")
+
+// // var num1 = 100;
+// // var num2 = 20;
+
+// // var total = num1 + num2 + 100;
+
+// // var sub = num1 - num2;
+
+
+// // var names = ["jahid", "Rahim", "Rana", "Rakib", 33, 23,]
+// // var info  = [23, 21, 43, 23]
+
+// // console.log (names[2])
+
+// // var names = ["jahid", "Rahim", "Rana", "Rakib", 33, 23,]
+// // // names[6] = " A+"
+
+// // // names.push("test")
+
+// // names.pop();
+
+
+
+// // names.unshift("abuldsdh")
+// // names.shift("abuldsdh")
+// // console.log(names)
+
+
+// // // x = y ;
+
+// // // console.log("abc123"=="abc123 ")
+
+// // // console.log(10 === "10")
+
+// // // var num1 = 10;
+// // // var num2 = 10;
+
+// // // console.log(num1 !== num2);
+// // // console.log(! true);
+
+
+// // if (regPass === logPass && registrationEmail === logEmail ){
+// //   console.log('email thik aache');
+// // }
+// // else{
+// //   console.log('email thik nai');
+// // }
+
+// var marks = 80;
+// var age =20;
+
+// if ( marks >= 80 && marks <= 100 ){
+ 
+//     if(age = 20){
+//       console.log("apni amr theke tk niben");
+//     }
+//     else{
+//       console.log("A+");
+//     }
+// }
+// else if(marks >= 70 && marks <= 79){
+//   console.log("A");
+// }
+// else if(marks >= 60 && marks <= 69){
+//   console.log("B+");
+// }
+// else if (marks >= 50 && marks <= 59 ){
+//   console.log("B");
+// }
+// else if (marks >= 40 && marks <= 49 ){
+//   console.log("c");
+// } 
+// else {
+//   if(marks > 100 ){
+//     console.log("Makrs beshi hoiche");
+//   }
+//   else if( marks < 0 ){
+//     console.log("Makrs kom hoiche");
+//   }
+// }
+
+// var registrationEmail = 'demi@gmail.com';
+// var regPass = 'abc123';
+
+// var logEmail = 'demi@gmail.com';
+// var logPass = 'abc1231';
+
+
+// // regPass === logPass ? console.log("hellow") : console.log("no hellow")
+
+// var  output = regPass === logPass ? 'correct pass' : 'wng pass';
+// console.log(output)
+
+// for( var i = 0; i < 10; i++ ){
+//   console.log(i)
+// }
+
+// var names = ["rana", "school", "basa" ,"news"];
+
+// console.log(names.length);
+
+// // for( var i = 0; i <= names.length ; i++){
+// //   console.log(names[i]);
+
+// // }
+// var index = 0
+
+// while(index <= names.length){
+
+//     console.log("hellow");
+//     index++
+
+// }
+
+function add(num1, num2){
+
+  console.log(num1 + num2)
+
+}
+
+add(20, 30);
+
+add(30, 20)
+
+
+
+
+
