5932e7c10df468e0ee5511517c1c16e0970394b3
jahidpappuctg <jahidpappublog@gmail.com>
2022-12-01 10:21:53 +0600

    new

--- class-1.js
+++ class-1.js
@@ -1,133 +1,217 @@
-// // var  firstName = "Jahid";
+// // // var  firstName = "Jahid";
 
 
 
-// // console.log("name")
+// // // console.log("name")
 
-// // var num1 = 100;
-// // var num2 = 20;
+// // // var num1 = 100;
+// // // var num2 = 20;
 
-// // var total = num1 + num2 + 100;
+// // // var total = num1 + num2 + 100;
 
-// // var sub = num1 - num2;
-
-
-// // var names = ["jahid", "Rahim", "Rana", "Rakib", 33, 23,]
-// // var info  = [23, 21, 43, 23]
-
-// // console.log (names[2])
-
-// // var names = ["jahid", "Rahim", "Rana", "Rakib", 33, 23,]
-// // // names[6] = " A+"
-
-// // // names.push("test")
-
-// // names.pop();
+// // // var sub = num1 - num2;
 
 
+// // // var names = ["jahid", "Rahim", "Rana", "Rakib", 33, 23,]
+// // // var info  = [23, 21, 43, 23]
 
-// // names.unshift("abuldsdh")
-// // names.shift("abuldsdh")
-// // console.log(names)
+// // // console.log (names[2])
 
+// // // var names = ["jahid", "Rahim", "Rana", "Rakib", 33, 23,]
+// // // // names[6] = " A+"
 
-// // // x = y ;
+// // // // names.push("test")
 
-// // // console.log("abc123"=="abc123 ")
-
-// // // console.log(10 === "10")
-
-// // // var num1 = 10;
-// // // var num2 = 10;
-
-// // // console.log(num1 !== num2);
-// // // console.log(! true);
+// // // names.pop();
 
 
-// // if (regPass === logPass && registrationEmail === logEmail ){
-// //   console.log('email thik aache');
-// // }
-// // else{
-// //   console.log('email thik nai');
-// // }
 
-// var marks = 80;
-// var age =20;
+// // // names.unshift("abuldsdh")
+// // // names.shift("abuldsdh")
+// // // console.log(names)
 
-// if ( marks >= 80 && marks <= 100 ){
+
+// // // // x = y ;
+
+// // // // console.log("abc123"=="abc123 ")
+
+// // // // console.log(10 === "10")
+
+// // // // var num1 = 10;
+// // // // var num2 = 10;
+
+// // // // console.log(num1 !== num2);
+// // // // console.log(! true);
+
+
+// // // if (regPass === logPass && registrationEmail === logEmail ){
+// // //   console.log('email thik aache');
+// // // }
+// // // else{
+// // //   console.log('email thik nai');
+// // // }
+
+// // var marks = 80;
+// // var age =20;
+
+// // if ( marks >= 80 && marks <= 100 ){
  
-//     if(age = 20){
-//       console.log("apni amr theke tk niben");
-//     }
-//     else{
-//       console.log("A+");
-//     }
-// }
-// else if(marks >= 70 && marks <= 79){
-//   console.log("A");
-// }
-// else if(marks >= 60 && marks <= 69){
-//   console.log("B+");
-// }
-// else if (marks >= 50 && marks <= 59 ){
-//   console.log("B");
-// }
-// else if (marks >= 40 && marks <= 49 ){
-//   console.log("c");
-// } 
-// else {
-//   if(marks > 100 ){
-//     console.log("Makrs beshi hoiche");
-//   }
-//   else if( marks < 0 ){
-//     console.log("Makrs kom hoiche");
-//   }
-// }
+// //     if(age = 20){
+// //       console.log("apni amr theke tk niben");
+// //     }
+// //     else{
+// //       console.log("A+");
+// //     }
+// // }
+// // else if(marks >= 70 && marks <= 79){
+// //   console.log("A");
+// // }
+// // else if(marks >= 60 && marks <= 69){
+// //   console.log("B+");
+// // }
+// // else if (marks >= 50 && marks <= 59 ){
+// //   console.log("B");
+// // }
+// // else if (marks >= 40 && marks <= 49 ){
+// //   console.log("c");
+// // } 
+// // else {
+// //   if(marks > 100 ){
+// //     console.log("Makrs beshi hoiche");
+// //   }
+// //   else if( marks < 0 ){
+// //     console.log("Makrs kom hoiche");
+// //   }
+// // }
 
-// var registrationEmail = 'demi@gmail.com';
-// var regPass = 'abc123';
+// // var registrationEmail = 'demi@gmail.com';
+// // var regPass = 'abc123';
 
-// var logEmail = 'demi@gmail.com';
-// var logPass = 'abc1231';
+// // var logEmail = 'demi@gmail.com';
+// // var logPass = 'abc1231';
 
 
-// // regPass === logPass ? console.log("hellow") : console.log("no hellow")
+// // // regPass === logPass ? console.log("hellow") : console.log("no hellow")
 
-// var  output = regPass === logPass ? 'correct pass' : 'wng pass';
-// console.log(output)
+// // var  output = regPass === logPass ? 'correct pass' : 'wng pass';
+// // console.log(output)
 
-// for( var i = 0; i < 10; i++ ){
-//   console.log(i)
-// }
+// // for( var i = 0; i < 10; i++ ){
+// //   console.log(i)
+// // }
 
-// var names = ["rana", "school", "basa" ,"news"];
+// // var names = ["rana", "school", "basa" ,"news"];
 
-// console.log(names.length);
+// // console.log(names.length);
 
-// // for( var i = 0; i <= names.length ; i++){
-// //   console.log(names[i]);
+// // // for( var i = 0; i <= names.length ; i++){
+// // //   console.log(names[i]);
+
+// // // }
+// // var index = 0
+
+// // while(index <= names.length){
+
+// //     console.log("hellow");
+// //     index++
 
 // // }
-// var index = 0
 
-// while(index <= names.length){
+// // function add(num1, num2){
 
-//     console.log("hellow");
-//     index++
+// //   console.log(num1 + num2)
 
-// }
+// // }
 
-function add(num1, num2){
+// // add(20, 30);
 
-  console.log(num1 + num2)
+// // add(30, 20)
 
-}
+// // var names = ["rana", "school", "basa" ,"news"];
 
-add(20, 30);
+// // function printName(accept){
+// //     for( var i = 0; i < accept.length; i++){
+// //         console.log(accept[i]);
+// //     }
+// // }
 
-add(30, 20)
+// // printName(names);
+
+// // function add(a, b){
+// //   var sum = a + b;
+
+// //     return sum;
+// // }
 
 
+// // var total = add(10, 1);
 
+// // console.log(total);
+
+// // var obj = {
+
+// //     name : 'jahid',
+// //     roll : 20,
+// //     sub : ['bangla', 'bangla'],
+// //     info :{
+// //       schoolName : "bush"
+
+// //     },
+// //     add: function(a, b){
+// //       console.log(a + b);
+// //     }
+
+// // }
+
+// // console.log(obj);
+
+
+// // var info = {
+// //   title : 'Bangla song',
+// //   views :22002,
+// //   uplaod : '29/293/12',
+// //   channel : '1K',
+// //   dislike: 20,
+// // }
+
+// // // console.log(inof.title);
+
+// // var data = 'uplaod';
+
+// // console.log(info[data]);
+
+
+// var info =[
+//   {
+//     name : 'Jahid',
+//     roll : 20,
+//     reg  : 209393929202,
+//     sub  : ["bangla", "english","Match"],
+//     fird : [
+//       {
+//         name: "karim",
+//         age : "20",
+//         address : "save, dhaka"
+//       }
+//     ]
+//   },
+
+//   {
+//     name : 'Jahid',
+//     roll : 20,
+//     reg  : 209393929202,
+//     sub  : ["bangla", "english","Match"],
+//     fird : [
+//       {
+//         name: "karim",
+//         age : "20",
+//         address : "save, dhaka"
+//       }
+//     ]
+//   }
+// ]
+
+// // console.log(info[0].fird[0].address);
+// console.log(info[0].fird[0].name)
 
 
---
+++ html&javascript.js
@@ -0,0 +1,29 @@
+var heading = document.getElementsByClassName("heading");
+
+var change = heading[1];
+
+change.innerText = "jahid hossain"
+
+console.log(heading[0].innerText);
+
+
+
+
+
+heading[0].innerText ="change";
+
+// console.log(heading[0]);
+
+var inputBox = document.getElementById("inputBox");
+
+// console.log(inputBox);
+
+var headings = document.querySelectorAll(".heading");
+
+// console.log(headings[1]);
+
+var input = document.querySelector("#inputBox");
+
+console.log(input.value);
+
+
